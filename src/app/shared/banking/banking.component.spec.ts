c20526347e2614bd9bf67068eb23dad47fafd7ba
juvberry <julia.dvb@gmail.com>
2022-03-14 11:52:18 -0300

    imports adicionados nos specs, corrigindo o erro nullInjectorError

--- src/app/shared/banking/banking.component.spec.ts
+++ src/app/shared/banking/banking.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from 'src/app/app.component';
 
@@ -14,7 +15,8 @@ describe('BankingComponent', () => {
         AppComponent,
         BankingComponent,
         ListComponent,
-      ]
+      ],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
   });
